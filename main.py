d286d018d72a99a1f451d652492eed90a3047dbe
Kabier Chaudhari <kabierchaudhari@Kmac.local>
2021-05-09 12:40:21 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    name = input("What is your name? ")
+    age = input("What is your age? ")
+    print("Name:" + name)
+    print("Age:" + age)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+      main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
